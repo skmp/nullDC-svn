174d677c21aed0f30d5154e435dbfa77764ba876
drkIIRaziel <drkIIRaziel@d5fdb02f-ce0d-0410-91b1-fe443107cc8c>
2006-06-03 13:43:26 +0000

    Fixed vblank timing
    git-svn-id: file:///home/skmp/Desktop/public_svn/nullDC@219 d5fdb02f-ce0d-0410-91b1-fe443107cc8c

--- trunk/nulldc/plugins/nullPvr/nullPvr/icPvr.cpp
+++ trunk/nulldc/plugins/nullPvr/nullPvr/icPvr.cpp
@@ -102,12 +102,10 @@ struct icPluginfo
 icPluginfo*cur_icpl;
 
 u8 pvr_regs[1*1024*1024];
-s32 clcount=0,clc_pvr_scanline=0;
+s32 clcount=0;
 u32 prv_cur_scanline=0;
 
 u32 fps;
-s32 pvr_numscanlines_clk=521;
-s32 pvr_numscanlines=521;
 
 u32 lasft_fps;
 
@@ -137,68 +135,66 @@ u32 ints=0;
 int vblk_cnt=0;
 double spd_fps=0;
 double spd_cpu=0;
+
+//u32 vblLine	= (pvrCycles / (vblCount * 7));	// Current Line
+void vblank_done()
+{
+	vblk_cnt++;
+
+	//redraw screenie
+	if (cur_icpl->PvrUpdate)
+		cur_icpl->PvrUpdate(1);
+
+	if ((timeGetTime()-(double)lasft_fps)>800)
+	{
+		spd_fps=(double)fps/(double)((double)(timeGetTime()-(double)lasft_fps)/1000);
+		spd_cpu=(double)vblk_cnt/(double)((double)(timeGetTime()-(double)lasft_fps)/1000);
+		spd_cpu*=(DCclock/1000000)/60;
+
+		ints=0;
+		lasft_fps=timeGetTime();
+		fps=0;
+
+		double fullfps=(spd_fps/spd_cpu)*200;
+
+		char fpsStr[256];
+		sprintf(fpsStr," FPS: %4.2f(%4.2f)  -  Sh4: %4.2f mhz (%4.2f%%) - nullDC v0.0.1", spd_fps,fullfps, spd_cpu,spd_cpu*100/200);
+		SetWindowText((HWND)Hwnd, fpsStr);
+		vblk_cnt=0;
+	}
+}
+void hblank_done(int scanline)
+{
+	prv_cur_scanline=scanline;
+	//RaiseInterrupt(holly_HBLank);//that is bad , its not allways raised , needs fixin
+
+	u32 data=*(u32*)&pvr_regs[0xa05f80cc & 0x7FFF];
+	if ((data & 0x3FFF) == prv_cur_scanline)
+		RaiseInterrupt(holly_SCANINT1);
+	if (((data >> 16) & 0x3FFF) == prv_cur_scanline)
+		RaiseInterrupt(holly_SCANINT2);
+}
+
+int last_scanline=-6789;
+#define SPG_LOAD_addr			0x005F80D8		//RW	HV counter load value
 void icUpdatePvr (u32 cycles)
 {
 	clcount+=cycles;
-	//RaiseInterrupt(InteruptID::
-	if (clcount>(DCclock)/60)
+	int pixel_clock=((*(int*)&pvr_regs[SPG_LOAD_addr & 0x7FFF]) & 0x3FF) +1;
+	int scanline=clcount/(pixel_clock*16);
+
+	if (clcount > (DCclock/60))//60 ~herz = 200 mhz / 60=3333333.333 cycles per screen refresh
 	{
-		vblk_cnt++;
-		clcount-=(DCclock)/60;//faked
-		//ok .. here , after much effort , we reached a full screen redraw :P
-		//now , we will copy everything onto the screen (meh) and raise a vblank interupt -- there is no vblank interrupt :P
-		RaiseInterrupt(InterruptID::holly_HBLank);// -> This turned out to be HBlank btw , needs to be emulated ;(
-		if (cur_icpl->PvrUpdate)
-			cur_icpl->PvrUpdate(1);
-
-			// didn't look at read i guess this is not needed 
-		//*(u32*)&pvr_regs[0x5F810C &0x7fff] |= 0x2000;	// SPG_STATUS
-//		u32 maple_vblnk_mode=ReadMem(SB_MDTSEL,4);
-	//	if (maple_vblnk_mode&0x1)
-	//	{
-		//	printf("MAPLE VBLANK MODE = 1");
-	//	}
-		if ((timeGetTime()-(double)lasft_fps)>800)
-		{
-			spd_fps=(double)fps/(double)((double)(timeGetTime()-(double)lasft_fps)/1000);
-			spd_cpu=(double)vblk_cnt/(double)((double)(timeGetTime()-(double)lasft_fps)/1000);
-			spd_cpu*=(DCclock/1000000)/60;
-
-			ints=0;
-			lasft_fps=timeGetTime();
-			fps=0;
-
-			double fullfps=(spd_fps/spd_cpu)*200;
-
-			char fpsStr[256];
-			sprintf(fpsStr," FPS: %4.2f(%4.2f)  -  Sh4: %4.2f mhz (%4.2f%%) - nullDC v0.0.1", spd_fps,fullfps, spd_cpu,spd_cpu*100/200);
-			SetWindowText((HWND)Hwnd, fpsStr);
-			vblk_cnt=0;
-		}
-		
-	}/* else	// this isn't fast enough, have to do it in read itself
-	{
-		*(u32*)&pvr_regs[0x5F810C &0x7fff] &= 0x1FFF;	// SPG_STATUS
-	}*/
-
-
-	clc_pvr_scanline += (int)cycles;
-	if (clc_pvr_scanline > ((DCclock/60) / pvr_numscanlines))//60 ~herz = 200 mhz / 60=3333333.333 cycles per screen refresh
+		vblank_done();
+		clcount-=DCclock/60;
+	}
+	if (scanline!=last_scanline)
 	{
 		//ok .. here , after much effort , we did one line
 		//now , we must chekc for raster beam pos interupts
-		prv_cur_scanline=(prv_cur_scanline+1)%pvr_numscanlines;
-
-	//	u32 data = *SPG_VBLANK_INT;
-		//u32 data = ReadMem(0xa05f80cc,4);
-		u32 data=*(u32*)&pvr_regs[0xa05f80cc & 0x7FFF];
-		if ((data & 0x3FFF) == prv_cur_scanline)
-			RaiseInterrupt(holly_SCANINT1);
-		if (((data >> 16) & 0x3FFF) == prv_cur_scanline)
-			RaiseInterrupt(holly_SCANINT2);
-		
-		clc_pvr_scanline -= ((DCclock/60) / pvr_numscanlines);
+		hblank_done(scanline);
 	}
+	last_scanline=scanline;
 }
 #define Is_64_Bit(addr) ((Address &0x1000000)==0)
 
@@ -222,21 +218,6 @@ void PvrSQWrite (u32*data,u32 len)
 	if (cur_icpl->PvrCommand)
 		cur_icpl->PvrCommand(data,len);
 }
-void icWriteMem (u32 Address,u32 data,u32 len)
-{
-	/*vramlock_Test(Address,0,0);
-	Address =vramlock_ConvAddrtoOffset64(Address);
-
-	if (len==1)
-		pvr_vram_b[Address & 0x7FFFFF]=(u8)data;
-	else if (len==2)
-		pvr_vram_w[(Address & 0x7FFFFF)>>1]=(u16)data;
-	else if (len==4)
-		pvr_vram_dw[(Address & 0x7FFFFF)>>2]=data;
-	else*/
-		printf("SysWriteMem: Size is %x, witch is not 1,2 or 4\n",len);
-}
-
 
 u32 vbl_last=0, vbl_mask=0xFFFFFFFF;;
 
@@ -339,27 +320,21 @@ void icEnqueue_IRQ	(u32 type)
 			RaiseInterrupt(holly_RENDER_DONE);
 			RaiseInterrupt(holly_RENDER_DONE_vd);
 			RaiseInterrupt(holly_RENDER_DONE_isp);
-			ints|=1;
 			break;
 		case OPAQUE_LIST :
 			RaiseInterrupt(holly_OPAQUE);
-			ints|=2;
 			break;
 		case OPAQUE_MOD_LIST :
 			RaiseInterrupt(holly_OPAQUEMOD);
-			ints|=4;
 			break;
 		case TRANS_LIST :
 			RaiseInterrupt(holly_TRANS);
-			ints|=8;
 			break;
 		case TRANS_MOD_LIST :
 			RaiseInterrupt(holly_TRANSMOD);
-			ints|=16;
 			break;
 		case PUNCH_THRU_LIST :
 			RaiseInterrupt(holly_PUNCHTHRU);
-			ints|=32;
 			break;
 		default:
 			//dc.dcon.WriteLine("DLL:InteruptHandler");
@@ -430,8 +405,8 @@ bool icInit ()
 	if (cur_icpl->PvrOpen)
 		cur_icpl->PvrOpen();
 
-	icWriteMem_reg(0xA05F8060,0,4);
-return true;
+	//icWriteMem_reg(0xA05F8060,0,4);
+	return true;
 }
 
 void icTerm ()
