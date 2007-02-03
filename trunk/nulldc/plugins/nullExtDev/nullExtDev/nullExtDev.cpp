0529c86b5e40a5b32e3ca6a63fc525eeff70e174
drkIIRaziel <drkIIRaziel@d5fdb02f-ce0d-0410-91b1-fe443107cc8c>
2007-02-03 05:24:19 +0000

    --Ported to new plugin spec (yrly ..)
    git-svn-id: file:///home/skmp/Desktop/public_svn/nullDC@435 d5fdb02f-ce0d-0410-91b1-fe443107cc8c

--- trunk/nulldc/plugins/nullExtDev/nullExtDev/nullExtDev.cpp
+++ trunk/nulldc/plugins/nullExtDev/nullExtDev/nullExtDev.cpp
@@ -6,32 +6,32 @@
 //"Emulates" the "nothing atached to ext.dev" :p
 
 //006* , on area0
-u32 ReadMem_A0_006(u32 addr,u32 size)
+u32 FASTCALL ReadMem_A0_006(u32 addr,u32 size)
 {
 	return 0;
 }
-void WriteMem_A0_006(u32 addr,u32 data,u32 size)
+void FASTCALL WriteMem_A0_006(u32 addr,u32 data,u32 size)
 {
 }
 //010* , on area0
-u32 ReadMem_A0_010(u32 addr,u32 size)
+u32 FASTCALL ReadMem_A0_010(u32 addr,u32 size)
 {
 	return 0;
 }
-void WriteMem_A0_010(u32 addr,u32 data,u32 size)
+void FASTCALL WriteMem_A0_010(u32 addr,u32 data,u32 size)
 {
 }
 //Area 5
-u32 ReadMem_A5(u32 addr,u32 size)
+u32 FASTCALL ReadMem_A5(u32 addr,u32 size)
 {
 	return 0;
 }
-void WriteMem_A5(u32 addr,u32 data,u32 size)
+void FASTCALL WriteMem_A5(u32 addr,u32 data,u32 size)
 {
 }
 
 //~ called every 1.5k cycles
-void Update(u32 cycles)
+void FASTCALL Update(u32 cycles)
 {
 }
 
@@ -46,69 +46,81 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 }
 
 
-void cfgdlg(PluginType type,void* window)
+void FASTCALL cfgdlg(void* window)
 {
 //	MessageBox((HWND)window,"Nothing to configure","nullExtDev plugin",MB_OK | MB_ICONINFORMATION);
 }
 
-//Give to the emu info for the plugin type
-EXPORT void dcGetPluginInfo(plugin_info* info)
-{
-	info->InterfaceVersion.full=PLUGIN_I_F_VERSION;
-	strcpy(info->Name,"nullExtDev (" __DATE__ ")");
-	info->PluginVersion.full=NDC_MakeVersion(MAJOR,MINOR,BUILD);
-	
-	info->Init=dcInit;
-	info->Term=dcTerm;
-	info->Reset=dcReset;
-
-	info->ThreadInit=dcThreadInit;
-	info->ThreadTerm=dcThreadTerm;
-	info->ShowConfig=cfgdlg;
-	info->Type=PluginType::ExtDevice;
-}
-
-//Give to the emu pointers for the PowerVR interface
-EXPORT void dcGetExtDeviceInfo(ext_device_plugin_if* info)
-{
-	info->InterfaceVersion.full=EXTDEVICE_PLUGIN_I_F_VERSION;
-	info->ReadMem_A0_006=ReadMem_A0_006;
-	info->WriteMem_A0_006=WriteMem_A0_006;
-	
-	info->ReadMem_A0_010=ReadMem_A0_010;
-	info->WriteMem_A0_010=WriteMem_A0_010;
-
-	info->ReadMem_A5=ReadMem_A5;
-	info->WriteMem_A5=WriteMem_A5;
-
-	info->UpdateExtDevice=Update;
-}
-
 
 //called when plugin is used by emu (you should do first time init here)
-void dcInit(void* param,PluginType type)
+s32 FASTCALL Load(emu_info* param)
 {
-	ext_device_init_params* params=(ext_device_init_params*)param;
+	return rv_ok;
 }
 
 //called when plugin is unloaded by emu , olny if dcInit is called (eg , not called to enumerate plugins)
-void dcTerm(PluginType type)
+void FASTCALL Unload()
 {
 
 }
 
 //It's suposed to reset anything 
-void dcReset(bool Manual,PluginType type)
+void FASTCALL edReset(bool Manual)
 {
 
 }
 
 //called when entering sh4 thread , from the new thread context (for any thread speciacific init)
-void dcThreadInit(PluginType type)
+s32 FASTCALL edInit(ext_device_init_params* p)
 {
+	return rv_ok;
 }
 
 //called when exiting from sh4 thread , from the new thread context (for any thread speciacific de init) :P
-void dcThreadTerm(PluginType type)
+void FASTCALL edTerm()
 {
+}
+
+//Give to the emu info for the plugin type
+void EXPORT_CALL dcGetPluginInfo(plugin_info* info)
+{
+	info->InterfaceVersion=PLUGIN_I_F_VERSION;
+	info->count=1;
+}
+
+//Give to the emu pointers for the PowerVR interface
+bool EXPORT_CALL dcGetPlugin(u32 id,plugin_info_entry* info)
+{
+	#define c info->common 
+	#define ed info->ext_dev
+
+	c.Type=PluginType::ExtDevice;
+	c.InterfaceVersion=PLUGIN_I_F_VERSION;
+
+	strcpy(c.Name,"nullExtDev (" __DATE__ ")");
+	c.PluginVersion=NDC_MakeVersion(MAJOR,MINOR,BUILD);
+	
+	c.Load=Load;
+	c.Unload=Unload;
+	
+
+	ed.Init=edInit;
+	ed.Reset=edReset;
+	ed.Term=edTerm;
+
+	ed.ShowConfig=cfgdlg;
+	
+
+	ed.ReadMem_A0_006=ReadMem_A0_006;
+	ed.WriteMem_A0_006=WriteMem_A0_006;
+	
+	ed.ReadMem_A0_010=ReadMem_A0_010;
+	ed.WriteMem_A0_010=WriteMem_A0_010;
+
+	ed.ReadMem_A5=ReadMem_A5;
+	ed.WriteMem_A5=WriteMem_A5;
+
+	ed.UpdateExtDevice=Update;
+
+	return true;
 }
--- trunk/nulldc/plugins/nullExtDev/nullExtDev/nullExtDev.h
+++ trunk/nulldc/plugins/nullExtDev/nullExtDev/nullExtDev.h
@@ -8,25 +8,11 @@
 #include <string.h>
 
 #define BUILD 0
-#define MINOR 1
-#define MAJOR 0
+#define MINOR 0
+#define MAJOR 1
 
 #define DCclock (200*1000*1000)
 
-//called when plugin is used by emu (you should do first time init here)
-void dcInit(void* param,PluginType type);
-
-//called when plugin is unloaded by emu , olny if dcInit is called (eg , not called to enumerate plugins)
-void dcTerm(PluginType type);
-
-//It's suposed to reset anything 
-void dcReset(bool Manual,PluginType type);
-
-//called when entering sh4 thread , from the new thread context (for any thread speciacific init)
-void dcThreadInit(PluginType type);
-
-//called when exiting from sh4 thread , from the new thread context (for any thread speciacific de init) :P
-void dcThreadTerm(PluginType type);
 
 #define 	ReadMemArrRet(arr,addr,sz)				\
 			{if (sz==1)								\
