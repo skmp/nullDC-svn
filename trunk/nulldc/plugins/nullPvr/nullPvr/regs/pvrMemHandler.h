424692ef56bf1f1b025269b9e0b39dff1cefb3d5
drkIIRaziel <drkIIRaziel@d5fdb02f-ce0d-0410-91b1-fe443107cc8c>
2007-03-25 18:04:52 +0000

    Removed file/folder
    git-svn-id: file:///home/skmp/Desktop/public_svn/nullDC@479 d5fdb02f-ce0d-0410-91b1-fe443107cc8c

--- trunk/nulldc/plugins/nullPvr/nullPvr/regs/pvrMemHandler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "..\nullPvr.h"
-
-//TODO : move to plugin
-
-u32 plugin_pvr_readreg_PowerVR(u32 addr,u32 sz);
-void plugin_pvr_writereg_PowerVR(u32 addr,u32 data,u32 sz);
-u32 plugin_pvr_readreg_TA(u32 addr,u32 sz);
-void plugin_pvr_writereg_TA(u32 addr,u32 data,u32 sz);
