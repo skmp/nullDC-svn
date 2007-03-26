04d184b8d2f1041ee90b2ba507d6838bd2d30243
drkIIRaziel <drkIIRaziel@d5fdb02f-ce0d-0410-91b1-fe443107cc8c>
2007-03-26 16:22:53 +0000

    --stores vertex info
    git-svn-id: file:///home/skmp/Desktop/public_svn/nullDC@493 d5fdb02f-ce0d-0410-91b1-fe443107cc8c

--- trunk/nulldc/plugins/nullPVR/nullPVR/ta_vdec.cpp
+++ trunk/nulldc/plugins/nullPVR/nullPVR/ta_vdec.cpp
@@ -6,6 +6,18 @@ using namespace TASplitter;
 u32 VertexCount=0;
 extern u32 FrameCount;
 
+Vertex verts[128*1024];
+u32 pplist_op_size;
+PolyParam pplist_op[48*1024];
+u32 pplist_pt_size;
+PolyParam pplist_pt[16*1024];
+u32 pplist_tr_size;
+PolyParam pplist_tr[16*1024];
+
+PolyParam* current_pp;
+u32* current_pp_count;
+Vertex* current_vert;
+
 //Fill that in w/ some vertex decoding :)
 struct VertexDecoder
 {
@@ -13,16 +25,31 @@ struct VertexDecoder
 	__forceinline
 		static void StartList(u32 ListType)
 	{
-
+		if (ListType==ListType_Opaque)
+		{
+			current_pp=&pplist_op[pplist_op_size];
+			current_pp_count=&pplist_op_size;
+		}
+		else if (ListType==ListType_Punch_Through)
+		{
+			current_pp=&pplist_pt[pplist_pt_size];
+			current_pp_count=&pplist_pt_size;
+		}
+		else if (ListType==ListType_Translucent)
+		{
+			current_pp=&pplist_tr[pplist_tr_size];
+			current_pp_count=&pplist_tr_size;
+		}
 	}
 	__forceinline
 		static void EndList(u32 ListType)
 	{
-
+		current_pp=0;
 	}
 
 	//Polys
-#define glob_param_bdc
+#define glob_param_bdc (*current_pp_count)++; \
+		current_pp->first=VertexCount;
 
 	__forceinline
 		static void fastcall AppendPolyParam0(TA_PolyParam0* pp)
@@ -64,16 +91,21 @@ struct VertexDecoder
 	__forceinline
 		static void StartPolyStrip()
 	{
-
+		glob_param_bdc;
 	}
 	__forceinline
 		static void EndPolyStrip()
 	{
-
+		current_pp->len=VertexCount - current_pp->first +1;
+		current_pp++;
 	}
 
 	//Poly Vertex handlers
-#define vert_cvt_base VertexCount++;
+#define vert_cvt_base VertexCount++; \
+	current_vert->x=vtx->xyz[0];\
+	current_vert->y=vtx->xyz[1];\
+	current_vert->z=vtx->xyz[2];\
+	
 
 
 	//(Non-Textured, Packed Color)
@@ -81,6 +113,10 @@ struct VertexDecoder
 		static void AppendPolyVertex0(TA_Vertex0* vtx)
 	{
 		vert_cvt_base;
+		
+		current_vert->argb = vtx->BaseCol;
+
+		current_vert++;
 	}
 
 	//(Non-Textured, Floating Color)
@@ -88,6 +124,10 @@ struct VertexDecoder
 		static void AppendPolyVertex1(TA_Vertex1* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert->argb = 0xFFFFFFFF;
+
+		current_vert++;
 	}
 
 	//(Non-Textured, Intensity)
@@ -95,6 +135,10 @@ struct VertexDecoder
 		static void AppendPolyVertex2(TA_Vertex2* vtx)
 	{
 		vert_cvt_base;
+		
+		current_vert->argb = 0xFFFFFFFF;
+
+		current_vert++;
 	}
 
 	//(Textured, Packed Color)
@@ -102,6 +146,10 @@ struct VertexDecoder
 		static void AppendPolyVertex3(TA_Vertex3* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert->argb = vtx->BaseCol;
+
+		current_vert++;
 	}
 
 	//(Textured, Packed Color, 16bit UV)
@@ -109,6 +157,10 @@ struct VertexDecoder
 		static void AppendPolyVertex4(TA_Vertex4* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert->argb = vtx->BaseCol;
+
+		current_vert++;
 	}
 
 	//(Textured, Floating Color)
@@ -116,11 +168,15 @@ struct VertexDecoder
 		static void AppendPolyVertex5A(TA_Vertex5A* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert->argb = 0xFFFFFFFF;
+
 	}
 	__forceinline
 		static void AppendPolyVertex5B(TA_Vertex5B* vtx)
 	{
 
+		current_vert++;
 	}
 
 	//(Textured, Floating Color, 16bit UV)
@@ -128,11 +184,14 @@ struct VertexDecoder
 		static void AppendPolyVertex6A(TA_Vertex6A* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert->argb = 0xFFFFFFFF;
 	}
 	__forceinline
 		static void AppendPolyVertex6B(TA_Vertex6B* vtx)
 	{
 
+		current_vert++;
 	}
 
 	//(Textured, Intensity)
@@ -140,6 +199,10 @@ struct VertexDecoder
 		static void AppendPolyVertex7(TA_Vertex7* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert->argb = 0xFFFFFFFF;
+
+		current_vert++;
 	}
 
 	//(Textured, Intensity, 16bit UV)
@@ -147,6 +210,10 @@ struct VertexDecoder
 		static void AppendPolyVertex8(TA_Vertex8* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert->argb = 0xFFFFFFFF;
+
+		current_vert++;
 	}
 
 	//(Non-Textured, Packed Color, with Two Volumes)
@@ -154,6 +221,8 @@ struct VertexDecoder
 		static void AppendPolyVertex9(TA_Vertex9* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert++;
 	}
 
 	//(Non-Textured, Intensity,	with Two Volumes)
@@ -161,6 +230,10 @@ struct VertexDecoder
 		static void AppendPolyVertex10(TA_Vertex10* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert->argb = 0xFFFFFFFF;
+
+		current_vert++;
 	}
 
 	//(Textured, Packed Color,	with Two Volumes)	
@@ -168,11 +241,14 @@ struct VertexDecoder
 		static void AppendPolyVertex11A(TA_Vertex11A* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert->argb = vtx->BaseCol0;
 	}
 	__forceinline
 		static void AppendPolyVertex11B(TA_Vertex11B* vtx)
 	{
 
+		current_vert++;
 	}
 
 	//(Textured, Packed Color, 16bit UV, with Two Volumes)
@@ -180,11 +256,14 @@ struct VertexDecoder
 		static void AppendPolyVertex12A(TA_Vertex12A* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert->argb = vtx->BaseCol0;
 	}
 	__forceinline
 		static void AppendPolyVertex12B(TA_Vertex12B* vtx)
 	{
 
+		current_vert++;
 	}
 
 	//(Textured, Intensity,	with Two Volumes)
@@ -192,11 +271,14 @@ struct VertexDecoder
 		static void AppendPolyVertex13A(TA_Vertex13A* vtx)
 	{
 		vert_cvt_base;		
+
+		current_vert->argb = 0xFFFFFFFF;
 	}
 	__forceinline
 		static void AppendPolyVertex13B(TA_Vertex13B* vtx)
 	{
 
+		current_vert++;
 	}
 
 	//(Textured, Intensity, 16bit UV, with Two Volumes)
@@ -204,11 +286,14 @@ struct VertexDecoder
 		static void AppendPolyVertex14A(TA_Vertex14A* vtx)
 	{
 		vert_cvt_base;
+
+		current_vert->argb = 0xFFFFFFFF;
 	}
 	__forceinline
 		static void AppendPolyVertex14B(TA_Vertex14B* vtx)
 	{
 
+		current_vert++;
 	}
 
 	//Sprites
@@ -269,11 +354,22 @@ struct VertexDecoder
 	__forceinline
 		static void ListInit()
 	{
+		pplist_op_size=0;
+		pplist_tr_size=0;
+		pplist_pt_size=0;
+		VertexCount=0;
+		current_vert = &verts[0];
 
 	}
 	__forceinline
 		static void SoftReset()
 	{
+		pplist_op_size=0;
+		pplist_tr_size=0;
+		pplist_pt_size=0;
+		VertexCount=0;
+		current_vert = &verts[0];
+
 	}
 };
 
--- trunk/nulldc/plugins/nullPVR/nullPVR/ta_vdec.h
+++ trunk/nulldc/plugins/nullPVR/nullPVR/ta_vdec.h
@@ -6,3 +6,27 @@ void ResetRenderer(bool Manual);
 void ListCont();
 void ListInit();
 void SoftReset();
+
+struct Vertex
+{
+	f32 x,y,z;
+
+	//we may want to switch to fp colors for speed
+	u32 argb;
+};
+
+struct PolyParam
+{
+	u32 first;
+	u32 len;
+
+	//TCW/TSP/Stuff here
+};
+
+extern Vertex verts[128*1024];
+extern u32 pplist_op_size;
+extern PolyParam pplist_op[48*1024];
+extern u32 pplist_pt_size;
+extern PolyParam pplist_pt[16*1024];
+extern u32 pplist_tr_size;
+extern PolyParam pplist_tr[16*1024];
