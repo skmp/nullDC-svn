58eca99fe55231f1727a9c1353f3f47eef314e26
drkIIRaziel <drkIIRaziel@d5fdb02f-ce0d-0410-91b1-fe443107cc8c>
2007-08-05 22:29:50 +0000

    --Gui moved to dll
    git-svn-id: file:///home/skmp/Desktop/public_svn/nullDC@604 d5fdb02f-ce0d-0410-91b1-fe443107cc8c

--- trunk/nulldc/nullDC/gui/base.cpp
+++ trunk/nulldc/nullDC/gui/base.cpp
@@ -5,7 +5,11 @@
 #include "plugins/plugin_manager.h"
 
 #include <windows.h>
-#include "emuWinUI.h"
+#include "base.h"
+#include "config/config.h"
+#include "dc/mem/Elf.h"
+#include "dc/gdrom/gdrom_if.h"
+#include "profiler/profiler.h"
 
 //LRESULT CALLBACK WinProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 //int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hprevinstance,LPSTR lpcmdline,int nshowcmd);
@@ -13,27 +17,506 @@
 //HWND mainhwnd;
 //HINSTANCE hinst;
 
-bool CreateGUI()
+MenuIDList MenuIDs;
+cDllHandler gui;
+/*
+
+typedef s32 EXPORT_CALL dbgReadMemFP(u32 addr,u32 sz,void* dst);
+typedef s32 EXPORT_CALL dbgWriteMemFP(u32 addr,u32 sz,void* dst);
+typedef int EXPORT_CALL guiMsgBoxFP(char* text,int type);
+
+typedef bool EXPORT_CALL EmuStartedFP();	//returns if emulation is started.
+
+typedef bool EXPORT_CALL EmuInitFP();
+
+typedef bool EXPORT_CALL EmuStartFP(); //returns false if it failed.It can fail if non inited and implicit init failed.
+typedef void EXPORT_CALL EmuStopFP();
+typedef void EXPORT_CALL EmuResetFP(bool Manual);	//well duh it resets =P.
+
+typedef bool EXPORT_CALL EmuStepFP(); //returns false if it failed.It can fail if non inited or allready running.
+typedef bool EXPORT_CALL EmuSkipFP(); //returns false if it failed.It can fail if non inited or allready running.
+
+typedef void EXPORT_CALL EmuSetPatchFP(u32 Value,u32 Mask);	//Enable/Disable a patch (system area hooks)
+
+
+typedef bool EXPORT_CALL EmuBootHLEFP();	//Copies the bin file from the disc , descrambles it if needed and sets up needed regs for boot.False if it failed.
+typedef bool EXPORT_CALL EmuLoadBinaryFP(char* file,u32 address);	//Loads a binary on the address.If the file is elf the address is ingored and the elf's
+														//offsets are used
+typedef bool EXPORT_CALL EmuSelectPluginsFP();	//Request the emu to show the select plugins interface
+typedef void EXPORT_CALL EmuStartProfilerFP();	//Start the TBP
+typedef void EXPORT_CALL EmuStopProfilerFP();	//Stop the TBP
+
+typedef void EXPORT_CALL DissasembleOpcodeFP(u16 opcode,u32 pc,char* Dissasm);
+typedef u32 EXPORT_CALL Sh4GetRegisterFP(u32 reg);
+typedef void EXPORT_CALL Sh4SetRegisterFP(u32 reg,u32 value);
+typedef int EXPORT_CALL GetSymbNameFP(u32 address,char *szDesc,bool bUseUnkAddress);
+*/
+
+s32 EXPORT_CALL b_dbgReadMem(u32 addr,u32 sz,void* dst)
 {
-	if (uiInit()!=UI_OK)
+	u8* ptr=(u8*)dst;
+	while(sz--)
+		*ptr++=ReadMem8(addr++);
+	return rv_ok;
+}
+s32 EXPORT_CALL b_dbgWriteMem(u32 addr,u32 sz,void* dst)
+{
+	return rv_ok;
+}
+
+bool EXPORT_CALL b_EmuStarted()	//returns if emulation is started.
+{
+	return sh4_cpu->IsCpuRunning();
+}
+
+bool EXPORT_CALL b_EmuInit()
+{
+	return Init_DC();
+}
+
+bool EXPORT_CALL b_EmuStart() //returns false if it failed.It can fail if non inited and implicit init failed.
+{
+	bool init=Init_DC();
+	if (init==false)
 		return false;
 
+	Start_DC();
 	return true;
+}
+void EXPORT_CALL b_EmuStop()
+{
+	Stop_DC();
+}
+bool EXPORT_CALL b_EmuReset(bool Manual)	//well duh it resets =P.
+{
+	return Reset_DC(Manual);
+}
+
+bool EXPORT_CALL b_EmuStep() //returns false if it failed.It can fail if non inited or allready running.
+{
+	if(!IsDCInited() || sh4_cpu->IsCpuRunning())
+		return false;
+	
+	sh4_cpu->Step();
+	return true;
+}
+bool EXPORT_CALL b_EmuSkip() //returns false if it failed.It can fail if non inited or allready running.
+{
+	if(!IsDCInited() || sh4_cpu->IsCpuRunning())
+		return false;
+	
+	sh4_cpu->Skip();
+	return true;
+}
+
+void EXPORT_CALL b_EmuSetPatch(u32 Value,u32 Mask)	//Enable/Disable a patch (system area hooks)
+{
+	SetPatches(Value,Mask);
+}
+
+
+bool EXPORT_CALL b_EmuBootHLE()	//Copies the bin file from the disc , descrambles it if needed and sets up needed regs for boot.False if it failed.
+{
+	return gdBootHLE();
+}
+bool EXPORT_CALL b_EmuLoadBinary(char* file,u32 address)	//Loads a binary on the address.If the file is elf the address is ingored and the elf's
+{
+	return LoadBinfileToSh4Mem(address,file)!=0;
+}													//offsets are used
+bool EXPORT_CALL b_EmuSelectPlugins()	//Request the emu to show the select plugins interface
+{
+	return plugins_Select();
+}
+void EXPORT_CALL b_EmuStartProfiler()	//Start the TBP
+{
+	start_Profiler();
+}
+void EXPORT_CALL b_EmuStopProfiler()	//Stop the TBP
+{
+	stop_Profiler();
+}
+
+void EXPORT_CALL b_DissasembleOpcode(u16 opcode,u32 pc,char* Dissasm)
+{
+	DissasembleOpcode(opcode,pc,Dissasm);
+}
+u32 EXPORT_CALL b_Sh4GetRegister(u32 reg)
+{
+	return sh4_cpu->GetRegister((Sh4RegType)reg);
+}
+void EXPORT_CALL b_Sh4SetRegister(u32 reg,u32 value)
+{
+	sh4_cpu->SetRegister((Sh4RegType)reg,value);
+}
+int EXPORT_CALL b_GetSymbName(u32 address,char *szDesc,bool bUseUnkAddress)
+{
+	return GetSymbName(address,szDesc,bUseUnkAddress);
+}
+
+#define maple_sett(x,y) "Current_" "maple" #x "_" #y
+#define maple_groop(x) maple_sett(x,0), maple_sett(x,1), maple_sett(x,2), maple_sett(x,3), maple_sett(x,4), maple_sett(x,5)
+
+char* ndc_snames[]=
+{
+	"Current_" "PVR",
+	"Current_" "GDR",
+	"Current_" "AICA",
+	"Current_" "ExtDevice",
+
+	maple_groop(0),
+	maple_groop(1),
+	maple_groop(2),
+	maple_groop(3),
+};
+
+#undef maple_sett
+#undef maple_groop
+
+#define _esai_(n,v,ma) 		case NDCS_##n:*tptr=settings.##v; break;
+void SwitchCpu()
+{
+	if((settings.dynarec.Enable==0 && sh4_cpu->ResetCache==0) ||
+	   (settings.dynarec.Enable!=0 && sh4_cpu->ResetCache!=0)  )
+	{
+		return;//nothing to do ...
+	}
+	
+	bool bStart=false;
+
+	if (sh4_cpu)
+	{
+		if (sh4_cpu->IsCpuRunning())
+		{
+			bStart=true;
+			Stop_DC();
+			sh4_cpu->Term();
+		}
+	}
+	if(settings.dynarec.Enable)
+	{
+		sh4_cpu=Get_Sh4Recompiler();
+		printf("Switched to Recompiler\n");
+	}
+	else
+	{
+		sh4_cpu=Get_Sh4Interpreter();
+		printf("Switched to Interpreter\n");
+	}
+	
+
+	if (bStart)
+		Start_DC();
+}
+s32 EXPORT_CALL GetEmuSetting(u32 sid,void* value)
+{
+	char* sptr=(char*)value;
+	u32* tptr=(u32*)value;
+	switch(sid)
+	{
+		
+	_esai_(DYNAREC_ENABLED,dynarec.Enable,1);
+	_esai_(DYNAREC_CPPASS,dynarec.CPpass,1);
+	_esai_(DYNAREC_UCFPU,dynarec.UnderclockFpu,1);
+
+	_esai_(DREAMCAST_CABLE,dreamcast.cable,3);
+	_esai_(DREAMCAST_RTC,dreamcast.RTC,0xFFFFFFFF);
+
+	_esai_(EMULATOR_ASTART,emulator.AutoStart,1);
+
+	default:
+		if (sid<NDCS_COUNT)
+		{
+			cfgLoadStr("nullDC_plugins",ndc_snames[sid-NDCS_PLUGIN_PVR],sptr,0);
+			return rv_ok;
+		}
+		else
+			return rv_error;
+	}
+
+	return rv_ok;
+}
+#undef _esai_
+
+#define _esai_(n,v,ma) 		case NDCS_##n:settings.##v=min(*tptr,ma);SaveSettings();
+s32 EXPORT_CALL SetEmuSetting(u32 sid,void* value)
+{
+	char* sptr=(char*)value;
+	u32* tptr=(u32*)value;
+	
+	switch(sid)
+	{	
+		_esai_(DYNAREC_ENABLED,dynarec.Enable,1);
+		SwitchCpu();//switch cpu if needed
+		break;
+		
+		_esai_(DYNAREC_CPPASS,dynarec.CPpass,1);
+		if (sh4_cpu->ResetCache)
+			sh4_cpu->ResetCache();
+		break;
+		
+		_esai_(DYNAREC_UCFPU,dynarec.UnderclockFpu,1);
+		if (sh4_cpu->ResetCache)
+			sh4_cpu->ResetCache();
+		break;
+
+		_esai_(DREAMCAST_CABLE,dreamcast.cable,3);
+		break;
+
+		_esai_(DREAMCAST_RTC,dreamcast.RTC,0xFFFFFFFF);
+		break;
+
+		_esai_(EMULATOR_ASTART,emulator.AutoStart,1);
+		break;
+
+	default:
+		if (sid<NDCS_COUNT)
+		{
+			cfgSaveStr("nullDC_plugins",ndc_snames[sid-NDCS_PLUGIN_PVR],sptr);
+			return rv_ok;
+		}
+		else
+			return rv_error;
+	}
+
+	return rv_ok;
+}
+char about_text[] = "Credits :" "\r\n"
+				" drk||Raziel \t: Main coder" "\r\n"
+				" ZeZu \t\t: Main coder" "\r\n"
+				" GiGaHeRz \t: Plugin work/misc stuff"  "\r\n"
+				" PsyMan \t\t: Mental support, managment,""\r\n"
+				"        \t\t  beta testing & everything else"  "\r\n"
+				" Xant \t\t: www & forum, beta testing"   "\r\n"
+				"\r\n"
+				"Beta testing :"  "\r\n"
+				" emwearz, Miretank, gb_away, Raziel, General Plot,"  "\r\n"
+				" Refraction, Ckemu,Falcon4ever, ChaosCode"  "\r\n"
+				"\r\n"
+				"Many thanks to :" "\r\n"
+				"Ector, Jim Denson, Flea, Jupi, Chankast team" "\r\n"
+				"and everyone else we forgot" "\r\n"
+				"\r\n"
+				"Hate list:" "\r\n"
+				"Xylene, for trying to mess channels by impersonating repsected people..." "\r\n"
+				" dont worry, you'l never get as good as they are.""\r\n"
+				"\r\n"
+				;
+char* EXPORT_CALL GetAboutText()
+{
+	return about_text;
+}
+PluginInfoList* EXPORT_CALL b_GetPluginList(u32 Type)
+{
+	List<PluginLoadInfo>* lst= GetPluginList((PluginType)Type);
+
+	PluginInfoList* rt=0,*rc=0,*rv=0;
+	for (int i=0;i<lst->itemcount;i++)
+	{
+		rc = new PluginInfoList();
+		
+		strcpy(rc->Name,(*lst)[i].Name);
+		rc->Version=(*lst)[i].PluginVersion;
+		strcpy(rc->dll,(*lst)[i].dll);
+
+		if(rt)
+			rt->next=rc;
+		else
+			rv=rc;
+
+		rt=rc;
+	}
+
+	delete lst;
+
+	return rv;
+}
+PluginInfoList* EXPORT_CALL b_GetMapleDeviceList(u32 DeviceType)
+{
+	List<MapleDeviceDefinition>* lst= GetMapleDeviceList((MapleDeviceType)DeviceType);
+
+	PluginInfoList* rt=0,*rc=0,*rv=0;
+	for (int i=0;i<lst->itemcount;i++)
+	{
+		rc = new PluginInfoList();
+		
+		strcpy(rc->Name,(*lst)[i].Name);
+		rc->Version=(*lst)[i].PluginVersion;
+		strcpy(rc->dll,(*lst)[i].dll);
+		rc->Flags=(*lst)[i].Flags;
+
+		if(rt)
+			rt->next=rc;
+		else
+			rv=rc;
+
+		rt=rc;
+	}
+
+	delete lst;
+
+	return rv;
+}
+s32 EXPORT_CALL b_FreePluginList(PluginInfoList* list)
+{
+	if (!list)
+		return rv_error;
+	while(list)
+	{
+		PluginInfoList* next=list->next;
+		delete list;
+		list=next;
+	}
+	return rv_ok;
+}
+void EXPORT_CALL b_EmuResetCaches()
+{
+	if (sh4_cpu && sh4_cpu->ResetCache)
+		sh4_cpu->ResetCache();
+}
+bool OpenAndLoadGUI(char* file)
+{
+	if (!gui.Load(file))
+	{
+		msgboxf("Unable to open gui dll (%s)",MBX_ICONERROR,file);
+		return false;
+	}
+
+	ndcGetInterfaceFP* gi=(ndcGetInterfaceFP*)gui.GetProcAddress("ndcGetInterface");
+
+	if (!gi)
+	{
+		msgboxf("Unable to resolve %s:ndcGetInterface",MBX_ICONERROR,file);
+		return false;
+	}
+	gi(&libgui);
+
+	gui_emu_info gpi;
+
+	gpi.ConfigExists=cfgExists;
+	gpi.ConfigLoadInt=cfgLoadInt;
+	gpi.ConfigLoadStr=cfgLoadStr;
+	gpi.ConfigSaveInt=cfgSaveInt;
+	gpi.ConfigSaveStr=cfgSaveStr;
+
+	gpi.dbgReadMem=b_dbgReadMem;
+	gpi.dbgWriteMem=b_dbgWriteMem;
+
+	gpi.EmuStarted=b_EmuStarted;
+
+	gpi.EmuInit = b_EmuInit ;
+
+	gpi.EmuStart = b_EmuStart ;
+	gpi.EmuStop = b_EmuStop ;
+	gpi.EmuStep = b_EmuStep ;
+	gpi.EmuSkip = b_EmuSkip ;
+
+	gpi.EmuSetPatch = b_EmuSetPatch ;
+	gpi.EmuReset = b_EmuReset ;
+	gpi.EmuResetCaches=b_EmuResetCaches;
+
+	gpi.EmuBootHLE = b_EmuBootHLE ;
+	gpi.EmuLoadBinary = b_EmuLoadBinary ;
+
+	gpi.EmuSelectPlugins = b_EmuSelectPlugins ;
+	gpi.EmuStartProfiler = b_EmuStartProfiler ;
+	gpi.EmuStopProfiler = b_EmuStopProfiler ;
+
+	gpi.DissasembleOpcode = b_DissasembleOpcode ;
+	gpi.Sh4GetRegister = b_Sh4GetRegister ;
+	gpi.Sh4SetRegister = b_Sh4SetRegister ;
+	gpi.GetSymbName = b_GetSymbName ;
+	
+	gpi.GetSetting=GetEmuSetting;
+	gpi.SetSetting=SetEmuSetting;
+	gpi.GetAboutText=GetAboutText;
+	
+	
+	gpi.GetPluginList=b_GetPluginList;
+	gpi.GetMapleDeviceList=b_GetMapleDeviceList;
+	gpi.FreePluginList=b_FreePluginList;
+
+	if (rv_ok != libgui.Load(&gpi))
+		return false;
+	
+	libgui.GetMenuIDs(&MenuIDs);
+
+	return true;
+}
+bool CreateGUI()
+{
+	char gui[128];
+	cfgLoadStr("nullDC_plugins","GUI",gui,"nullDC_GUI_Win32.dll");
+	if (!OpenAndLoadGUI(gui))
+	{
+		if (msgboxf("Do you want to load default gui ?",MBX_YESNO) == MBX_RV_YES)
+		{
+			if (!OpenAndLoadGUI("nullDC_GUI_Win32.dll"))
+				return false;
+			else
+				return true;
+		}
+		else
+			return false;
+	}
+	else
+		return true;
 }
 void DestroyGUI()
 {
 	plugins_Unload();
-	uiTerm();
+	
+	libgui.Unload();
+	gui.Unload();
 }
 void GuiLoop()
 {
-	while (uiMain()!=UI_MAIN_QUIT)
-		__noop;
+	libgui.Mainloop();
 
+	//Make sure emulation is stoped
 	Stop_DC();
 }
 
-void* GetRenderTargetHandle()
+void* EXPORT_CALL GetRenderTargetHandle()
 {
-	return g_hWnd;
+	return libgui.GetRenderTarget();
 }
+bool SelectPluginsGui()
+{
+	return libgui.SelectPluginsGui();
+}
+
+void EmuEventBroadcast()
+{
+}
+
+u32 PowerVR_menu;
+u32 GDRom_menu;
+u32 Aica_menu;
+u32 Maple_menu;
+u32 Maple_menu_ports[4][6];
+u32 ExtDev_menu;
+u32 Debug_menu;
+
+
+void SetMenuItemHandler(u32 id,MenuItemSelectedFP* h)
+{
+	MenuItem mi;
+	mi.Handler=h;
+	
+	libgui.SetMenuItem(id,&mi,MIM_Handler);
+}
+
+enum ProfilerInfoFlags
+{
+	PIF_TBP_ON=1,		//Time based profiler is enabled (so the info returned here is valid)
+	PIF_TBP_ONEX=2,		//Extra profile info is valid (block usage)
+
+	PIF_BMS_DTCS=4	,	//Dynarec Translation Cache stats (size;block counts)
+	PIF_BMS_LCS=8	,	//lookup cache stats (miss/hit)
+	PIF_BMS_CSGS=16	,	//call stack guess stats (miss/hit)
+	
+};
+struct profiler_info
+{
+	u32 flags;
+};
--- trunk/nulldc/nullDC/gui/base.h
+++ trunk/nulldc/nullDC/gui/base.h
@@ -1,9 +1,51 @@
 #pragma once
 #include "types.h"
 #include "dc/sh4/sh4_if.h"
+#include "plugins/plugin_manager.h"
 
 bool CreateGUI();
 void DestroyGUI();
 void GuiLoop();
 
-void* GetRenderTargetHandle();
+void* EXPORT_CALL GetRenderTargetHandle();
+bool SelectPluginsGui();
+void EmuEventBroadcast();
+
+#ifndef _MenuItemSelectedFP_
+#define _MenuItemSelectedFP_
+typedef void EXPORT_CALL MenuItemSelectedFP(u32 id,void* WindowHandle,void* user);
+
+
+enum MenuItemMask
+{
+	MIM_Text=1,
+	MIM_Handler=2,
+	MIM_Bitmap=4,
+	MIM_Style=8,
+	MIM_PUser=16,
+	MIM_All=0xFFFFFFFF,
+};
+struct MenuItem
+{
+	char* Text;			//Text of the menu item
+	MenuItemSelectedFP* Handler;	//called when the menu is clicked
+	void* Bitmap;		//bitmap handle
+	u32 Style;			//MIS_* combination
+	void* PUser;		//User defined pointer :)
+};
+#endif
+//void EXPORT_CALL DeleteAllMenuItemChilds(u32 id);
+void SetMenuItemHandler(u32 id,MenuItemSelectedFP* h);
+/*
+u32 EXPORT_CALL AddMenuItem(u32 parent,s32 pos,char* text,MenuItemSelectedFP* handler ,u32 checked);
+void EXPORT_CALL SetMenuItemStyle(u32 id,u32 style,u32 mask);
+void EXPORT_CALL GetMenuItem(u32 id,MenuItem* info,u32 mask);
+void EXPORT_CALL SetMenuItem(u32 id,MenuItem* info,u32 mask);
+void EXPORT_CALL DeleteMenuItem(u32 id);
+//These still exist , but are no longer given to plugins
+
+void EXPORT_CALL SetMenuItemBitmap(u32 id,void* hbmp);
+u32 EXPORT_CALL GetMenuItemStyle(u32 id);
+void* EXPORT_CALL GetMenuItemBitmap(u32 id);
+*/
+extern MenuIDList MenuIDs;
