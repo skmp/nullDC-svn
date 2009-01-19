86fcf44afed21298bdbde0194495c598eb01a39b
drkIIRaziel <drkIIRaziel@d5fdb02f-ce0d-0410-91b1-fe443107cc8c>
2009-01-19 20:02:40 +0000

    --added some more hacks to aica_hax !
    git-svn-id: file:///home/skmp/Desktop/public_svn/nullDC@860 d5fdb02f-ce0d-0410-91b1-fe443107cc8c

--- trunk/nulldc/plugins/EmptyAICA/aica_hax.cpp
+++ trunk/nulldc/plugins/EmptyAICA/aica_hax.cpp
@@ -61,11 +61,85 @@ void FASTCALL UpdateAICA(u32 Cycles)
 		}
 	}
 
+		*(u32*)&aica_ram[((0x80FFC0-0x800000)&0x1FFFFF)]=*(u32*)&aica_ram[((0x80FFC0-0x800000)&0x1FFFFF)]?0:3;
+	//return 0x3;			//hack snd_dbg
+
+	//the kos command list is 0x810000 to 0x81FFFF
+	//here we hack the first and last comands
+	//seems to fix everything ^^
+	*(u32*)&aica_ram[0x1000C]=0x1;
+
+	//aica_ram[0x1000C]+=0x1;
+
+	//here we hack the first and last comands
+	//seems to fix everything ^^
+	*(u32*)&aica_ram[0x1FFFC]=1;
+		//return 0x1;			//hack kos command que
+	//aica_ram[0x1FFFC]+=0x1;
+
+	//crazy taxi / doa2 /*
+	//if (addr>0x800100 && addr<0x800300)
+	//	return 0x800000;			//hack katana command que
+	for (int i=0x100;i<0x300;i+=4)
+	{
+		*(u32*)&aica_ram[i]=0x800000;
+	}
+	//if (calls & 0x10)
+		//memset(&aica_ram[0x100],0x800000,16);
+	*(u32*)&aica_ram[0x32040]=0xFFFFFF;
+		//return 0xFFFFFF;			//hack katana command que
+	*(u32*)&aica_ram[0x32060]=0xFFFFFF;
+		//return 0xFFFFFF;			//hack katana command que
+
+	//trickstyle 
+	//another position for command queue ?
+	//if (addr>=0x813400 && addr<=0x813900)
+	{
+		//what to return what to return ??
+		//this not works good
+		//return 0x1;
+	}
+	//memset(a&ica_ram[13400],0x800000,0x400);
+
+	//hack Katana wait
+	//Waits for something , then writes to the value returned here
+	//Found in Roadsters[also in Toy cmd]
+	//it seems it is somehow rlated to a direct mem read after that 
+	// im too tired to debug and find the actualy relation but it is
+	*(u32*)&aica_ram[0x000EC]=0x80000;
+		//return 0x80000;
+	*(u32*)&aica_ram[0x0005C]=0x80000;
+		//return 0x80000;
+	/*Rez, not realy working tho*/
+	/*static u32 tempyy=0xDEAD;
+	if (addr==0x8000F8)
+		return (tempyy++)^(tempyy^=-1);*/
+	//*(u32*)&aica_ram[0x000EC]=0x1;
+
+	//same as above , doa2/le
+	*(u32*)&aica_ram[0x000E8]=0x80000;
+		//return 0x80000;
+	//*(u32*)&aica_ram[0x000E8]=0x1;
+
+	//addr == 0x008000f8 -> recv , locks while reading from it ;)
+
+
+
+	//some status bits/queue , hacked of course :p
+	//if ((addr>0x8014e0) && (addr<0x8015e0))
+	//{
+	//	return 0xFFFFFF;
+	//}
+
+	for (int i=0x014e0;i<0x015e0;i+=4)
+	{
+		*(u32*)&aica_ram[i]=0xFFFFFF;
+	}
 	//if((addr&0xFF)==0x5C)
 	//	return 1;			// hack naomi aica ram check ?
 
 	//kos/katana
-	*(u32*)&aica_ram[((0x80FFC0-0x800000)&0x1FFFFF)]=*(u32*)&aica_ram[((0x80FFC0-0x800000)&0x1FFFFF)]?0:3;
+	//*(u32*)&aica_ram[((0x80FFC0-0x800000)&0x1FFFFF)]=*(u32*)&aica_ram[((0x80FFC0-0x800000)&0x1FFFFF)]?0:3;
 	//return 0x3;			//hack snd_dbg
 
 	//the kos command list is 0x810000 to 0x81FFFF
@@ -73,19 +147,19 @@ void FASTCALL UpdateAICA(u32 Cycles)
 	//seems to fix everything ^^
 	//if (addr==0x81000C)
 		//return 0x1;			//hack kos command que
-	aica_ram[0x1000C]+=0x1;
+	//aica_ram[0x1000C]+=0x1;
 
 	//here we hack the first and last comands
 	//seems to fix everything ^^
 	//if (addr==0x81FFFC)
 	//	return 0x1;			//hack kos command que
-	aica_ram[0x1FFFC]+=0x1;
+	//aica_ram[0x1FFFC]+=0x1;
 
 	//crazy taxi / doa2 /*
 	//if (addr>0x800100 && addr<0x800300)
 		//return 0x800000;			//hack katana command que
-	if (calls & 0x10)
-		memset(&aica_ram[0x100],0x800000,16);
+	//if (calls & 0x10)
+	//	memset(&aica_ram[0x100],0x800000,16);
 
 	//trickstyle 
 	//another position for command queue ?
@@ -104,12 +178,12 @@ void FASTCALL UpdateAICA(u32 Cycles)
 	// im too tired to debug and find the actualy relation but it is
 	//if (addr==0x8000EC)
 	//	return 0x80000;
-	*(u32*)&aica_ram[0x000EC]=0x1;
+	//*(u32*)&aica_ram[0x000EC]=0x1;
 
 	//same as above , doa2/le
 	//if (addr==0x8000E8)
 		//return 0x80000;
-	*(u32*)&aica_ram[0x000E8]=0x1;
+	//*(u32*)&aica_ram[0x000E8]=0x1;
 
 	//addr == 0x008000f8 -> recv , locks while reading from it ;)
 
